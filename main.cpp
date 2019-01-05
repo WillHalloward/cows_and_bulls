5adefcdb446dc1c0a46c7e370bd3b6f95745fb55
Halloward <willhalloward@gmail.com>
2019-01-05 05:31:32 +0100

    Cauldron fixes.

--- main.cpp
+++ main.cpp
@@ -8,83 +8,85 @@
 #include <ctime>
 #include <chrono>
 
+using namespace std;
+
 const int NUM_LEN = 4;
-const std::string NUMBERS = "1234567890";
+const string NUMBERS = "1234567890";
 
 //shuffle NUMBERS and grab the first 4
-std::string generate_digits() {
-    std::string answer = NUMBERS;
-    shuffle(answer.begin(), answer.end(), std::default_random_engine(std::chrono::system_clock::now().time_since_epoch().count()));
+string generate_digits() {
+    string answer = NUMBERS;
+    shuffle(answer.begin(), answer.end(), default_random_engine(chrono::system_clock::now().time_since_epoch().count()));
     answer = answer.substr(0, NUM_LEN);
-    std::cout << "secret number generated\n";
+    cout << "secret number generated\n";
     return answer;
 }
 
-bool check_dupe(std::string str) {
+bool check_dupe(string str) {
     sort(str.begin(), str.end());
     return adjacent_find(str.begin(), str.end()) != str.end();
 }
 
 //currently not used, will be needed later.
-std::string player_guess() {
-    std::string guess;
+string player_guess() {
+    string guess;
     //loop until we get a valid answer.
     while(true) {
-        std::cout << "Whats your guess?\n";
-        getline(std::cin, guess);
+        cout << "Whats your guess?\n";
+        getline(cin, guess);
         if (guess.length() != NUM_LEN ||
-            guess.find_first_not_of(NUMBERS) != std::string::npos ||
+            guess.find_first_not_of(NUMBERS) != string::npos ||
             check_dupe(guess)){
-            std::cout << "invalid input\n";
+            cout << "invalid input\n";
             continue;
         }
         return guess;
     }
 }
 
-bool calculate_score(const std::string &answer, std::string guess, std::pair<int, int> &cb){
+bool calculate_score(const string &answer, string guess, pair<int, int> &cb){
     cb.first = 0, cb.second = 0;
     for (int i = 0; i != NUM_LEN; i++){
         if (answer.find(guess[i]) == i){
             cb.second++;
         }
-        else if (answer.find(guess[i]) != std::string::npos){
+        else if (answer.find(guess[i]) != string::npos){
             cb.first++;
         }
     }
     return cb.second == NUM_LEN;
 }
 
-std::string generate_guess(std::vector<std::string> &list){
+string generate_guess(vector<string> &list){
     if( list.empty() ) return "";
     return list[rand() % list.size()];
 }
 
-void high_score(int turns, const std::string &number){
-    std::ofstream output;
-    output.open("High_score.txt", std::ios_base::app);
+void high_score(int turns, const string &number){
+    ofstream output;
+    output.open("High_score.txt", ios_base::app);
     output << "Turns: " << turns << "\nNumber: " << number << "\n============\n";
     output.close();
 }
 
-std::vector <std::string> generate_list(){
-    std::vector<std::string> number_vector;
+vector <string> generate_list(){
+    vector<string> number_vector;
     for (int x = pow(10, NUM_LEN-2); x < pow(10, NUM_LEN)-1; x++)
     {
         //to make sure we include 0123, 0124, etc
-        unsigned long long int number_of_zeros = NUM_LEN - std::to_string(x).length();
-        std::string temp = std::string(number_of_zeros, '0').append(std::to_string(x));
+        unsigned long long number_of_zeros = NUM_LEN - to_string(x).length();
+        string temp = string(number_of_zeros, '0').append(to_string(x));
         if(!check_dupe(temp)) {
             number_vector.push_back(temp);
         }
     }
-    std::cout << number_vector.size() << " - Number of possible guesses.\n";
+    cout << number_vector.size() << " - Number of possible guesses.\n";
     return number_vector;
 }
 
-void filter(const std::string &guess, std::pair<int, int> cb, std::vector<std::string> &list){
+void filter(const string &guess, pair<int, int> cb, vector<string> &list){
     auto it = list.begin();
-    std::pair<int, int> filter_cb;
+    pair<int, int> filter_cb;
     while(it != list.end()){
         calculate_score(*it, guess, filter_cb);
         if (filter_cb.first < cb.first || filter_cb.second < cb.second || *it == guess){
@@ -94,36 +96,36 @@ void filter(const std::string &guess, std::pair<int, int> cb, std::vector<std::s
             it++;
         }
     }
-    std::cout << list.size() << " - Number of possible guesses remaining\n";
+    cout << list.size() << " - Number of possible guesses remaining\n";
 }
 
 int main() {
-    std::pair <int, int> cows_bulls;
+    pair <int, int> cows_bulls;
     int turn = 0;
-    std::string answer = generate_digits();
-    std::vector<std::string> list = generate_list();
-    std::cout << answer << " - The secret answer.\n"; //cheat to make it easier to test.
+    string answer = generate_digits();
+    vector<string> list = generate_list();
+    cout << answer << " - The secret answer.\n"; //cheat to make it easier to test.
     while (!list.empty()) {
         turn++;
-        //std::string guess = player_guess();
-        std::string guess = generate_guess(list);
-        std::cout << guess << " - Guess\n";
+        //string guess = player_guess();
+        string guess = generate_guess(list);
+        cout << guess << " - Guess\n";
         if (calculate_score(answer, guess, cows_bulls)) {
             break;
         }
-        std::cout << "Turn:  " << turn << "\nCows:  " << cows_bulls.first << "\nBulls: " << cows_bulls.second << std::endl;
+        cout << "Turn:  " << turn << "\nCows:  " << cows_bulls.first << "\nBulls: " << cows_bulls.second << endl;
         filter(guess, cows_bulls, list);
-        std::cout << "========================================\n";
+        cout << "========================================\n";
     }
     if (cows_bulls.second == NUM_LEN) {
-        std::cout << "You win! the secret number was: " << answer << "\nIt took: " << turn << " turns\n";
+        cout << "You win! the secret number was: " << answer << "\nIt took: " << turn << " turns\n";
         high_score(turn, answer);
     }
     else if (list.empty()){
-        std::cout << "ye filthy cheat\n";
+        cout << "ye filthy cheat\n";
     }
     else{
-        std::cout << "something went horribly wrong\n";
+        cout << "something went horribly wrong\n";
     }
     return 0;
 }
