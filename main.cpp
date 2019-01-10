e95e2d9cb1566e75363a47f15f91cd94e863076e
Halloward <willhalloward@gmail.com>
2019-01-10 22:30:13 +0100

    Cleaned up code Moved cpu_cpu loop to function instead of main.

--- main.cpp
+++ main.cpp
@@ -8,19 +8,12 @@ int main() {
     while(mode < 1 || mode > 3){
         cin >> mode;
         switch (mode){
-            case 1: player_cpu(); break;
-            case 2: cpu_player(); break;
-            case 3:
-                int loops;
-                printf("How many times?\n");
-                while(!(cin >> loops) || loops <= 0) {
-                    std::cin.clear();
-                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                    std::cout << "Error, please try again.\n";
-                }
-                for (int i = 0; loops > i; i++) {
-                    cpu_cpu();
-                }; break;
+            case 1: player_cpu();
+                break;
+            case 2: cpu_player();
+                break;
+            case 3: cpu_cpu();
+                break;
             default: printf("Please type in a number between 1 and 3 to select your mode.\n");
         }
         cin.clear();
